fb2be76f6bc09345da06a2dfbadeb926228d6fc5
nadiaandyani <nadiaandyani@outlook.com>
2018-03-27 20:24:09 +1100

    add app.use in app.js to link css to ejs

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const router =  require('./routes/routes.js')
 app.set('view engine', 'ejs');
 app.use(router);
 
+app.use(express.static(__dirname + '/views'));
+
 app.listen(3000, function() {
     console.log('Server started at port 3000');
 });
