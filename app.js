cb17bc76cc5e709bfa0e8f8f67cdfb22a695802e
nadiaandyani <nadiaandyani@outlook.com>
2018-03-27 20:37:52 +1100

    save first merge

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const router =  require('./routes/home.js')
 
 app.set('view engine', 'ejs');
 app.use(router);
+app.use(express.static(__dirname + '/views'));
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, function(){
     console.log(`Express listening on port ${PORT}`);
-app.use(express.static(__dirname + '/views'));
+});
