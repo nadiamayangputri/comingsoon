99ad00e20d6ab4928a178ad48c6df437743b9165
Qianqian Zheng <audrey@Qianqian-Air.gateway>
2018-04-12 22:14:49 +1000

    updated routes and match game font

--- routes/home.js
+++ routes/home.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const controller = require('../controller/controller');
 
 
-router.get('/', controller.fetchMembers);
+router.get('/comingsoon', controller.fetchMembers);
 router.get('/games/match', controller.match_game);
 module.exports = router;
